ebc9f52956643f5f5af63f55f285885b1a68a543
“EmptyWYZ” <18211062609@163.com>
2021-03-15 22:18:38 +0800

    冒泡排序最low法

---
+++ practice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Solution{
+    public:
+        int sorting(int arr[], int len){
+
+            if(len == 0) return NULL;
+
+            for(int i = 0;i < len - 1; i++){
+                int tmp = 0;
+                for(int j = 0; j < len - i - 1; j++){
+                    if(arr[j] > arr[j + 1]){
+                        tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] =tmp;
+                    }
+                }
+            }
+            for(int i = 0; i < len - 1; i++){
+                cout << arr[i] << endl;
+            }
+            return 0;
+        }
+
+};
+int main() {
+
+    char str1[] = "hello world";
+    cout << str1 << endl;
+
+    return 0;
+}
