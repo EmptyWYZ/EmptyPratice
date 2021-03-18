8a304270d9505618a87e676673dfc75039336508
“EmptyWYZ” <18211062609@163.com>
2021-03-19 00:26:15 +0800

    add happy num func

--- hashPravtice.cpp
+++ hashPravtice.cpp
@@ -16,3 +16,29 @@ class Sloution{
             return vector<int>(ret_set.begin(), ret_set.end());
         }
 };
+class HappyNum{
+    public:
+        int getSum(int n){
+            int sum = 0;
+            while(n){
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            return sum;
+        }
+        bool happNum(int n){
+            unordered_set<int> arr;
+            while(1){
+                int sum = getSum(n);
+                if(sum == 1){
+                    return true;
+                }
+                if(arr.find(sum) != arr.end()){
+                    return false;
+                }else{
+                    arr.insert(sum);
+                }
+                n = sum;
+            }
+        }
+};
