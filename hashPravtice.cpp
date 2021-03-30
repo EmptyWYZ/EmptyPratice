64285ad259dbab5e936b2129ea62f89fa07f3f8d
“EmptyWYZ” <18211062609@163.com>
2021-03-31 00:11:24 +0800

    add fourcounter with double ptr

--- hashPravtice.cpp
+++ hashPravtice.cpp
@@ -150,3 +150,40 @@ public:
         return ret;
     }
 };
+class FourCount {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ret;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {//去重a
+                continue;
+            }
+            for(int j = i + 1; j < nums.size(); j++) { 
+                if(j > i + 1 && nums[j] == nums[j - 1]) {//去重b
+                    continue;
+                }
+                int left = j + 1;//双指针
+                int right = nums.size() - 1;
+                while(right > left) {
+                    if(nums[i] + nums[j] + nums[left] + nums[right] > target) {
+                        right--;
+                    }else if(nums[i] + nums[j] + nums[left] + nums[right] < target) {
+                        left++;
+                    }else {//去重
+                        ret.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while(right > left && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        while(right > left && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        right--;
+                        left++;
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+};
