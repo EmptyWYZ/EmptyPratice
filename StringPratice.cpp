ca659d3202257779625f61e1932e9613a8e369ad
“EmptyWYZ” <18211062609@163.com>
2021-04-06 23:51:50 +0800

    use double ptr to replaceSpace

--- StringPratice.cpp
+++ StringPratice.cpp
@@ -32,6 +32,32 @@ public:
         return s;
     }
 };
+class ReplaceSpace {
+public:
+    string replaceSpace(string s) {
+        //先扩充数组，再进行替换，双指针
+        int count = 0;
+        int oldSize = s.size();
+        for (int i = 0; i < oldSize; i++) {
+            if(s[i] == ' ') {
+                count++;
+            }
+        }
+        s.resize(s.size() + count * 2);
+        int newSize = s.size();
+        for(int i = newSize - 1, j = oldSize - 1; j < i; i--, j--) {
+            if(s[j] != ' ') {
+                s[i] = s[j];
+            }else {
+                s[i] = '0';
+                s[i - 1] = '2';
+                s[i - 2] = '%';
+                i -= 2;
+            }
+        }
+        return s;
+    }
+};
  int main(void) {
      cout<<"白尼屯万岁！"<<endl;
      return 0;
