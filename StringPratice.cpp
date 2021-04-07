c25e6edeb9977096da82e6afebe5d9abcc35a917
“EmptyWYZ” <18211062609@163.com>
2021-04-08 01:03:19 +0800

    add ReverseWords

--- StringPratice.cpp
+++ StringPratice.cpp
@@ -58,6 +58,55 @@ public:
         return s;
     }
 };
+class ReverseWords {
+public:
+    void reversestr(string& s, int start, int end) {
+        for(int i = start , j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+    void rmExtraSpace(string& s) {
+        for(int i = s.size() - 1; i > 0; i--) {
+            if(s[i] == s[i - 1] && s[i] == ' ') {
+                s.erase(s.begin() + i);//删除中间多余空格
+            }
+        }
+        if(s.size() > 0 && s[s.size() - 1] == ' ') {//删除后面多余空格
+            s.erase(s.begin() + s.size() - 1);
+        }
+        if(s.size() > 0 && s[0] == ' ') {
+            s.erase(s.begin());
+        }
+    }
+    string reverseWords(string s) {
+        //双指针法删除多余内存
+        rmExtraSpace(s);
+        // //反转字符串
+        reverse(s.begin(), s.end());
+        // reverse(s, 0, s.size() - 1);
+        // //反转单词，遇到空格停下来
+        int start = 0, end = 0;
+        bool flag = false;
+        for(int i = 0; i < s.size(); i++) {
+            if((!flag) || (s[i] != ' ' && s[i - 1] == ' ')) {//开始进入单词
+                start = i;
+                flag = true;
+            }
+            if(flag && (i == (s.size() - 1)) && s[i] != ' ') {//字符串的最后位置无空格
+                end = i;
+                reversestr(s, start, end);
+                flag = false;
+            }
+            if(flag && s[i] == ' ' && s[i - 1] != ' ') {//遇到空格停下来，单词结束
+                end = i - 1;
+                reversestr(s, start, end);
+                flag = false;
+            }
+
+        }
+        return s;
+    }
+};
  int main(void) {
      cout<<"白尼屯万岁！"<<endl;
      return 0;
