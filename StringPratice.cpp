25353f8f8680b6a0190dbc605d9c531358ea1c97
“EmptyWYZ” <18211062609@163.com>
2021-04-14 00:54:12 +0800

    add getValue

--- StringPratice.cpp
+++ StringPratice.cpp
@@ -162,6 +162,34 @@ public:
         return -1;
     }
 };
+class GetValue{
+public:
+    void getValue(int *next, const string &s) {
+        int j = -1;
+        next[0] = j;
+        for(int i = 1; i < s.size(); i++) {
+            while(j >= 0 && s[i] != s[j + 1]) {
+                j = next[j];
+            }
+            if(s[i] == s[j + 1]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+        if(s.size() == 0) {
+            return 0;
+        }
+        int next[s.size()];
+        getValue(next, s);
+        int len = s.size();
+        if(next[len - 1] != -1 && len % (len - (next[len - 1] + 1)) == 0){
+            return true;
+        }
+        return false;
+    }
+};
  int main(void) {
      cout<<"白尼屯万岁！"<<endl;
      return 0;
