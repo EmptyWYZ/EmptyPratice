22e01b43b6e90f2c44b3bbdebb0a8e622efa5733
“EmptyWYZ” <18211062609@163.com>
2021-04-15 00:16:24 +0800

    add new way to implement strstr()

--- StringPratice.cpp
+++ StringPratice.cpp
@@ -162,6 +162,42 @@ public:
         return -1;
     }
 };
+class Kmp2 {
+public:
+    void getNext(int *next, const string &s) {
+        int j = 0;
+        next[0] = j;
+        for(int i = 1; i < s.size(); i++) {
+            while(j > 0 && s[i] != s[j]) {
+                j = next[j - 1];
+            }
+            if(s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;//更新最长相同前后缀
+        }
+    }
+    int strStr(string haystack, string needle) {
+        if(needle.size() == 0) {
+            return 0;
+        }
+        int next[needle.size()];
+        getNext(next, needle);
+        int j = 0;
+        for(int i = 0;i < haystack.size(); i++) {
+            while(j > 0 && haystack[i] != needle[j]) {
+                j = next[j - 1];//若不匹配，查找前缀列表
+            }
+            if(haystack[i] == needle[j]) {
+                j++;
+            }
+            if(j == (needle.size())){//若最长匹配长度走到了needle的末尾
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
+};
 class GetValue{
 public:
     void getValue(int *next, const string &s) {
